d9c32b10550148b661264021e5ab40ece3be1bec
Uriel Brawer <150702873+uboddot@users.noreply.github.com>
2026-05-11 13:37:14 +0200

    Ci setup (#1)
    * ci-test
    
    * typo
    
    * saved
    
    * typo
    
    * test
    
    * basic setup
    
    ---------
    
    Co-authored-by: urielBrawer <uriel.brawer@students.fhnw.ch>

---
+++ alavaFrontend/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='content'>
+      <div className="title">Alava</div>
+      <div className='section'>
+        <div className='about'>
+          Place holder about.
+        </div>
+        <div className='offers'>
+          Place holder offers.
+        </div>
+        <div className='impressions'>
+          Place holder impressions.
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
