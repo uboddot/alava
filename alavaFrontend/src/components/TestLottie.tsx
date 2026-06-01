cbda72d5691c1a36723698cb956bd6a4645ec3a1
urielBrawer <uriel.brawer@students.fhnw.ch>
2026-06-01 16:01:48 +0200

    update TestLottie component to use new animation data from testLottie2.json

--- alavaFrontend/src/components/TestLottie.tsx
+++ alavaFrontend/src/components/TestLottie.tsx
@@ -1,11 +1,12 @@
 import { useLottie } from "lottie-react";
 import testLottie from "../assets/testLottie.json";
+import testLottie2 from "../assets/testLottie2.json";
 
 export { TestLottie };
 
 function TestLottie() {
     const options = {
-        animationData: testLottie,
+        animationData: testLottie2,
         loop: true
     };
     const { View } = useLottie(options);
